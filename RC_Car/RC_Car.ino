c96a2ff27ef2fe03363782554ea016166665b53b
Karim <karimmaximous@gmail.com>
2021-05-18 17:54:34 -0400

    Updated and refactored code after physical testing and debugging

--- RC_Car/RC_Car.ino
+++ RC_Car/RC_Car.ino
@@ -1,5 +1,3 @@
-#include <SoftwareSerial.h> //Import bluetooth library
-
 //To drive a motor to a direction, say, clockwise, the pin Input 1 must be high 
 //while the pin Input 2 must be low. To drive the motor counter clockwise, 
 //the pin Input 1 is low while the pin Input 2 is high.
@@ -7,7 +5,7 @@
 // Initialize front motors 1 and 2; Motor 1 is on the left and Motor 2 is on the right
 // When viewed from above the car with the car's front facing away from the observer
 // For speed control, must use PMW Pins: 3, 5, 6, 9, 10, 11
-int front_m1p1 = 1; // IN1
+int front_m1p1 = 25; // IN1
 int front_m1p2 = 2; // IN2
 int fm1_speed = 3; //Speed control for left front motor : ENA
 
@@ -25,30 +23,32 @@ int back_m2p1 = 10; //IN3
 int back_m2p2 = 12; //IN4
 int bm2_speed = 11; //Speed control for right back motor : ENB
 
+//LED used to visually see when the chip receives a signal when it lights up
+int led = 31; 
+
 //Initialize movement keyword variables
 String Forward = "Forward"; 
 String Back = "Back";
 String Stop = "Stop";
 String Right = "Right";
+String f_Right = "Front Right";
+String b_Right = "Back Right";
 String Left = "Left";
+String f_Left = "Front Left";
+String b_Left = "Back Left";
 
-//Initialize max, min, actual and reduced factor of the car (255 -> 5 volts)
+//Initialize max speed (255 -> 5 volts)
 //Actual speed -> speed received from bluetooth module
 int max_speed = 255;
-int min_speed = 0;
-int actual_speed = min_speed; //Car begins stationary
+int actual_speed = 0; //Car begins stationary
 
-/*
-// Delay values, can be altered and optimized during the testing phase
-int mini_delay = 100
-int short_delay = 300
-int medium_delay = 500
-int long_delay = 1000
-*/
 // Initialize bluetooth connection
-SoftwareSerial Blue(19,18); // RX pin 19; TX pin 18 -> arduino MEGA
-char blueData; //Bluetooth data will be stored here
+// RX pin 19; TX pin 18 -> arduino MEGA
 // RX pin receives data while TX pin transmits data
+char blueData; //Bluetooth data will be stored here
+char lock1;
+char lock2;
+char lock3;
 
 void setup() {
   // put your setup code here, to run once:
@@ -67,78 +67,78 @@ void setup() {
   pinMode(back_m2p1, OUTPUT);
   pinMode(back_m2p2, OUTPUT);
   pinMode(bm2_speed, OUTPUT);
-
-  speed_control(min_speed); //Set car to stationary at the beginning
-
-  //Set up bluetooth signal and begin reading
-  //Serial.begin(9600);
-  Blue.begin(9600); 
+  pinMode(led, OUTPUT);
+  
+  //Set up bluetooth signal on hardware serial and begin reading at 115200 baud
+  Serial1.begin(115200); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
- if (Blue.available()){
-  blueData = Blue.read();
-
-  if ((int) blueData >= 0 && (int) blueData <= 9){
+//The locks are used to eliminate the "S" signal received through the app.
+// The signal floods the chip and is therefore unable to smoothly read other signals
+// By eliminating this signal so that one-off "S" signals are not "read" the car can move smoothly
+ if (Serial1.available()){
+  lock1 = Serial1.read();
+  digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
+  if (lock1 == 'S'){
+    delay(50);
+    lock2 = Serial1.read();
+    if (lock2 == lock1){
+       delay(50);
+       lock3 = Serial1.read();
+       if (lock3 == lock2){
+        blueData = lock3;
+        }else{
+          blueData = lock3;
+          }
+      }else{
+        blueData = lock2;
+        }
+    }else{
+    blueData = lock1;
+  }
+  
+  if (((int(blueData)-int('0')) >= 0) && ((int(blueData)-int('0')) <= 9)){
     //Multiply by 10 to get speed, then by 2.55 to convert from 0->100 to 0->255
     //Add 0.5 for rounding purposes then change it to an int which truncates it
-    speed_control((int)((((float) blueData) * 25.5) + 0.5));
+    speed_control(int((int(blueData)-int('0')) * 25.5));
    }
    if (blueData == 'q'){
     speed_control(max_speed);
     }
     if (blueData == 'F'){ //Forward
       movement(Forward);
-      //Serial.println("Moving Forward now..");
-      delay(100);
     }
     if (blueData == 'G'){ //Forward Left
       turn(f_Left);
-      //Serial.println("Moving Forward and Left now..");
-      delay(100);
     }
     if (blueData == 'I'){ //Forward Right
       turn(f_Right);
-      //Serial.println("Moving Forward and Right now..");
-      delay(100);
     }
     if (blueData == 'B'){ //Back
        movement(Back);
-       //Serial.println("Moving Back now..");
-       delay(100);  
     }
     if (blueData == 'H'){ //Back Left
        turn(b_Left);
-       //Serial.println("Moving Back and Left now..");
-       delay(100);  
     }
     if (blueData == 'J'){ //Back Right
        turn(b_Right);
-       //Serial.println("Moving Back and Right now..");
-       delay(100);  
     }
     if (blueData == 'L'){ //Left
        turn(Left);
-       //Serial.println("Moving Left now..");
-       delay(100);  
     }
     if (blueData == 'R'){ //Right
        turn(Right);
-       //Serial.println("Moving Right now..");
-       delay(100);  
     }
     if ((blueData == 'D') or (blueData == 'S')){ //Stop or turn everything OFF
       movement(Stop);
-      //Serial.println("Stopped and stationary..");
-      delay(200);  
     }
     
-  }else{
-    //Serial.println("Attempting to receive a command..");
-    delay(100);
- }
+  }
 }
 
 //Function that changes the speed of the car wheels
@@ -148,8 +148,6 @@ void speed_control(int speed_in){
   analogWrite(bm1_speed, speed_in);
   analogWrite(bm2_speed, speed_in);
 }
-
-
 
 //Function that controls the movement of the car. Takes in inputs: Forward, Back and Stop
 void movement(String Mode){
